3462801eb6604492b40735ab7291a73b4362fac3
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-07-04 13:40:48 +0200

    Add CostAccountingAlg and TuplespaceAlg

---
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/accounting/CostAccountingAlg.scala
@@ -0,0 +1,24 @@
+package coop.rchain.rholang.interpreter.accounting
+
+import cats.effect.Sync
+import cats.mtl.MonadState
+import coop.rchain.rholang.interpreter.accounting
+
+trait CostAccountingAlg[F[_]] {
+  def charge(cost: Cost): F[Unit]
+  def getTotal: F[CostAccount]
+  def set(cost: CostAccount): F[Unit]
+}
+
+object CostAccountingAlg {
+  def monadState[F[_]](state: MonadState[F, CostAccount])(
+      implicit F: Sync[F]): CostAccountingAlg[F] =
+    new CostAccountingAlg[F] {
+
+      override def set(cost: CostAccount): F[Unit] = F.suspend(state.set(cost))
+
+      override def charge(cost: accounting.Cost): F[Unit] = state.modify(_ + cost)
+
+      override def getTotal: F[CostAccount] = F.suspend(state.get)
+    }
+}
