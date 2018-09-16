505bc9f321fd05701029f9f4d80ef26762c755b4
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-09-16 18:02:11 +0200

    RHOL-11: Rholang: Make `CostAccountingAlg` consistent across different constructors.

--- rholang/src/main/scala/coop/rchain/rholang/interpreter/accounting/CostAccountingAlg.scala
+++ rholang/src/main/scala/coop/rchain/rholang/interpreter/accounting/CostAccountingAlg.scala
@@ -14,36 +14,26 @@ trait CostAccountingAlg[F[_]] {
 }
 
 object CostAccountingAlg {
-  def apply[F[_]: Sync](init: CostAccount): F[CostAccountingAlg[F]] = Ref[F].of(init).map { state =>
-    new CostAccountingAlg[F] {
-      override def charge(cost: Cost): F[Unit] =
-        for {
-          _ <- failOnOutOfPhlo
-          _ <- state.update(_ - cost)
-          _ <- failOnOutOfPhlo
-        } yield ()
-      override def get: F[CostAccount]             = state.get
-      override def set(cost: CostAccount): F[Unit] = state.set(cost)
-
-      private val failOnOutOfPhlo: F[Unit] =
-        FlatMap[F].ifM(state.get.map(_.cost.value < 0))(Sync[F].raiseError(OutOfPhlogistonsError),
-                                                        Sync[F].unit)
-    }
-  }
+  def apply[F[_]: Sync](init: CostAccount): F[CostAccountingAlg[F]] =
+    Ref[F].of(init).map(state => new CostAccountingAlgImpl[F](state))
 
   def of[F[_]](implicit ev: CostAccountingAlg[F]): CostAccountingAlg[F] = ev
 
-  def unsafe[F[_]: Monad](initialState: CostAccount)(implicit F: Sync[F]): CostAccountingAlg[F] = {
-    val state = Ref.unsafe[F, CostAccount](initialState)
-    new CostAccountingAlg[F] {
+  def unsafe[F[_]: Monad](initialState: CostAccount)(implicit F: Sync[F]): CostAccountingAlg[F] =
+    new CostAccountingAlgImpl[F](Ref.unsafe[F, CostAccount](initialState))
 
-      override def set(cost: CostAccount): F[Unit] =
-        state.set(cost)
+  private class CostAccountingAlgImpl[F[_]](state: Ref[F, CostAccount])(implicit F: Sync[F])
+      extends CostAccountingAlg[F] {
+    override def charge(cost: Cost): F[Unit] =
+      for {
+        _ <- failOnOutOfPhlo
+        _ <- state.update(_ - cost)
+        _ <- failOnOutOfPhlo
+      } yield ()
+    override def get: F[CostAccount]             = state.get
+    override def set(cost: CostAccount): F[Unit] = state.set(cost)
 
-      override def charge(cost: accounting.Cost): F[Unit] =
-        state.update(_ - cost)
-
-      override def get(): F[CostAccount] = state.get
-    }
+    private val failOnOutOfPhlo: F[Unit] =
+      FlatMap[F].ifM(state.get.map(_.cost.value < 0))(F.raiseError(OutOfPhlogistonsError), F.unit)
   }
 }
